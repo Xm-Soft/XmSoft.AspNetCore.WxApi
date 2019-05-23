1d8c3a3aa4b780b8d48e93f93d1afd29b63a63c2
XmSoft <linxm_xm@outlook.com>
2019-05-23 21:42:28 +0800

    更新

---
+++ XmSoft.AspNetCore.WxApi/Response/QRCode/Account/WxApishorturlResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace XmSoft.AspNetCore.WxApi.Response.QRCode
+{
+    /// <summary>
+    /// 长链接转短链接接口 返回
+    /// </summary>
+    public class WxApiShortUrlResponse : WxApiResponse
+    {
+
+        /// <summary>
+        /// 	短链接。
+        /// </summary>
+        [JsonProperty(PropertyName = "short_url")]
+        public int Short_url { get; set; }
+
+        
+    }
+}
