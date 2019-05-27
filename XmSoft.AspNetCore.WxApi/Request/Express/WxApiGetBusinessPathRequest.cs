34cf1ce8125e9975b3f1933829f8589adcf7ce37
XmSoft <linxm_xm@outlook.com>
2019-05-28 00:58:46 +0800

    更新20190528

---
+++ XmSoft.AspNetCore.WxApi/Request/Express/WxApiGetBusinessPathRequest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XmSoft.AspNetCore.WxApi.Response.Express;
+
+namespace XmSoft.AspNetCore.WxApi.Request.Express
+{
+    /// <summary>
+    /// 查询运单轨迹
+    /// </summary>
+    public class WxApiGetBusinessPathRequest : IWxApiRequest<WxApiGetBusinessPathResponse>
+    {
+        /// <summary>
+        /// 查询运单轨迹
+        /// </summary>
+        public WxApiGetBusinessPathRequest()
+        {
+            
+        }
+        /// <summary>
+        /// Token
+        /// </summary>
+        public string AccessToken { get; set; }
+        /// <summary>
+        /// 订单 ID，需保证全局唯一
+        /// </summary>
+
+        public string Order_id { get; set; }
+        /// <summary>
+        ///用户 openid
+        /// </summary>
+        public string Openid { get; set; }
+        /// <summary>
+        /// 快递公司ID，参见getAllDelivery
+        /// </summary>
+        public string Delivery_id { get; set; }
+
+        /// <summary>
+        /// 运单ID
+        /// </summary>
+        public string Waybill_id { get; set; }
+
+        #region IWxApiRequest Members
+        /// <summary>
+        /// 请求路径
+        /// </summary>
+        /// <returns></returns>
+        public string GetRequestUrl()
+        {
+            return "https://api.weixin.qq.com/cgi-bin/express/business/path/get";
+        }
+        /// <summary>
+        /// 获取参数
+        /// </summary>
+        /// <returns></returns>
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new WxApiDictionary
+            {
+                { "access_token", AccessToken },
+                { "order_id", Order_id },
+                { "openid", Openid },
+                { "delivery_id", Delivery_id },
+                {"waybill_id",Waybill_id }
+            };
+            return parameters;
+        }
+        /// <summary>
+        /// 请求方式Post Or Get
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPost()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ XmSoft.AspNetCore.WxApi/Response/Express/WxApiGetBusinessPathResponse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmSoft.AspNetCore.WxApi.Response;
+
+namespace XmSoft.AspNetCore.WxApi.Response.Express
+{
+    /// <summary>
+    /// 查询运单轨迹 返回数据
+    /// </summary>
+    public class WxApiGetBusinessPathResponse:WxApiResponse
+    {
+        /// <summary>
+        /// 用户openid
+        /// </summary>
+        public string openid { get; set; }
+        /// <summary>
+        /// 快递公司 ID
+        /// </summary>
+        public string delivery_id { get; set; }
+        /// <summary>
+        /// 运单 ID
+        /// </summary>
+        public string waybill_id { get; set; }
+        /// <summary>
+        /// 轨迹节点数量
+        /// </summary>
+        public int path_item_num { get; set; }
+        /// <summary>
+        /// 轨迹节点列表
+        /// </summary>
+        public List<PathItemList> path_item_list { get; set; }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PathItemList
+    {
+        /// <summary>
+        /// 轨迹节点 Unix 时间戳
+        /// </summary>
+        public int action_time { get; set; }
+        /// <summary>
+        /// 轨迹节点类型
+        /// </summary>
+        public int action_type { get; set; }
+        /// <summary>
+        /// 轨迹节点详情
+        /// </summary>
+        public string action_msg { get; set; }
+    }
+}
